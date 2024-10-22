e25b2b69ce4021781c3efe41041877b4b9009de3
“vukas86” <vukasbit@gmail.com>
2024-10-22 22:53:04 +0200

    rebase git changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import header from "./Header.module.css";
+
+function Header() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default Header;
